119f16d0fa15a5d34e035b686488862d09daadc2
Maxime MARTIN <maxime.martin@st.com>
2023-01-28 14:52:54 +0100

    feat: plot clusted wafermap with color by cluster_id

--- wafermap.py
+++ wafermap.py
@@ -1,11 +1,13 @@
 import json
 from pathlib import Path
 import time
+from typing import List
 import numpy as np
 from sklearn.cluster import DBSCAN
 
 from klarf_reader.klarf import Klarf
 from wafermap_plot.wafermap import WaferMapPlot
+from wafermap_plot.models.defect_point import DefectPoint
 from models.cluseting_config import ClusteringConfig
 
 
@@ -29,20 +31,39 @@ path = Path("tests") / "assets" / "J247LFS_8625.000"
 
 content = Klarf.load_from_file(filepath=path)
 
-defect_ids = [defect.id for defect in content.wafers[0].defects]
-defect_points = [
-    (defect.point[0] / 1000, defect.point[1] / 1000)
-    for defect in content.wafers[0].defects
-]
+for wafer in content.wafers:
+    tic = time.time()
 
-wafermap = WaferMapPlot.plot(defect_points=defect_points)
-wafermap.show()
+    defect_ids = [defect.id for defect in wafer.defects]
+    defect_points = [
+        (defect.point[0] / 1000, defect.point[1] / 1000) for defect in wafer.defects
+    ]
 
+    clustering = DBSCAN(eps=configs.eps, min_samples=configs.min_samples).fit(
+        defect_points
+    )
 
-clustering = DBSCAN(eps=configs.eps, min_samples=configs.min_samples).fit(defect_points)
+    clustering_values, clusters = np.column_stack(
+        (np.array(defect_ids), clustering.labels_)
+    ), np.unique(clustering.labels_, axis=0)
 
-tic = time.time()
-clustering_values, number_of_cluster = np.column_stack(
-    (np.array(defect_ids), clustering.labels_)
-), np.unique(clustering.labels_, axis=0)
-tac = time.time()
+    clustered_defect_points: List[DefectPoint] = []
+    for cluster in clusters:
+        tmp_defect_id = [cd[0] for cd in clustering_values if cd[1] == cluster]
+        clustered_defect_points = [
+            *clustered_defect_points,
+            *[
+                DefectPoint(
+                    defect_id=defect.id,
+                    point=(defect.point[0] / 1000, defect.point[1] / 1000),
+                    bin=cluster,
+                )
+                for defect in wafer.defects
+                if defect.id in tmp_defect_id
+            ],
+        ]
+
+    wafermap = WaferMapPlot.plot(defect_points=clustered_defect_points)
+    wafermap.show()
+
+    tac = time.time()
